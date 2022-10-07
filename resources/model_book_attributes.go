50fd2a5bdb708d5cfaabf0f9a8d2594db6483264
slbmax <slobodzian.maxim@gmail.com>
2022-10-07 18:02:58 +0300

    fixed docs

---
+++ resources/model_book_attributes.go
@@ -0,0 +1,14 @@
+/*
+ * GENERATED. Do not modify. Your changes might be overwritten!
+ */
+
+package resources
+
+type BookAttributes struct {
+	// Book description
+	Description string `json:"description"`
+	// Book price ($)
+	Price int32 `json:"price"`
+	// Book title
+	Title string `json:"title"`
+}
---
+++ resources/model_book_relationships.go
@@ -0,0 +1,10 @@
+/*
+ * GENERATED. Do not modify. Your changes might be overwritten!
+ */
+
+package resources
+
+type BookRelationships struct {
+	Banner Media `json:"banner"`
+	File   Media `json:"file"`
+}
