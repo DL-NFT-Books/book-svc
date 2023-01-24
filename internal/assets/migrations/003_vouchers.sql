f8f6e5103802f77db665447d6c86171e8be7af16
anastasiia.lishchenko <anastasiia.lishchenko@gmail.com>
2023-01-24 15:43:03 +0200

    Change default values in db

--- internal/assets/migrations/003_vouchers.sql
+++ internal/assets/migrations/003_vouchers.sql
@@ -1,8 +1,8 @@
 -- +migrate Up
 
 ALTER TABLE book
-ADD column voucher_token VARCHAR(60) DEFAULT '',
-ADD column voucher_token_amount VARCHAR(40) DEFAULT '';
+ADD column voucher_token VARCHAR(60) DEFAULT '0x0000000000000000000000000000000000000000',
+ADD column voucher_token_amount VARCHAR(40) DEFAULT '0';
 
 -- +migrate Down
 
